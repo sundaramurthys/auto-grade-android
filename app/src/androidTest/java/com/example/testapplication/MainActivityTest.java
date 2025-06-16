f071352604dc67ea5485f8f309e5cf5ec3a151bd
sundaramurthys <sundaramurthys@bitsathy.ac.in>
2025-06-16 15:14:20 +0530

    first commit

---
+++ app/src/androidTest/java/com/example/testapplication/MainActivityTest.java
@@ -0,0 +1,25 @@
+package com.example.testapplication;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.*;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void welcomeTextIsCorrect() {
+        onView(withId(R.id.welcomeText))
+                .check(matches(withText("Hello, John!")));
+    }
+}
